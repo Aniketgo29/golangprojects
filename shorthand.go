3394c8e0bdb034a91392fc91441fcd7fd184fe8f
Aniketgo29 <aniketgolang@gmail.com>
2025-09-07 09:45:01 +0530

    day2

---
+++ shorthand.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	a := "aniket"
+	b := 32
+	c := 3.98
+	e := 4.3
+	d := true
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c+e)
+	fmt.Println(d)
+}
---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main () {
+
+	var a string = "John Doe"
+	fmt.Println(a)
+	var b int = 24
+	fmt.Println(b)
+	var c float64 = 154.63
+	fmt.Println(c)
+	var d bool = true
+	fmt.Println(d)
+	
+}
